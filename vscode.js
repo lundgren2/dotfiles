0268ba43c49d2d3e48e0e81d56d83ddf926519f0
Tobias Lundgren <tobias@lundgrendesign.se>
2018-11-29 18:13:05 +0100

    Add vscode settings

---
+++ vscode.js
@@ -0,0 +1,24 @@
+{
+  "editor.tabSize": 2,
+  "editor.rulers": [80],
+  "editor.cursorBlinking": "solid",
+  "editor.cursorWidth": 4,
+  "editor.wordWrap": "on",
+  "atomKeymap.promptV3Features": true,
+  "editor.multiCursorModifier": "ctrlCmd",
+  "editor.fontLigatures": true,
+  "editor.fontFamily": "SF Mono, Roboto Mono, Dank Mono,   Menlo, 'Courier New', Monaco, monospace",
+  "editor.fontSize": 14,
+  "editor.formatOnSave": true,
+  "javascript.validate.enable": false,
+  "eslint.autoFixOnSave": true,
+  "emmet.triggerExpansionOnTab": true,
+  "emmet.includeLanguages": {
+    "javascript": "javascriptreact",
+    "vue-html": "html",
+    "plaintext": "jade",
+    "blade": "html"
+  },
+  "editor.renderWhitespace": "all",
+  "workbench.colorTheme": "Framer Dark"
+}
