9fba2c6e4fa9af6a56e49d064838c8b66539842f
Tobias Lundgren <tobias@lundgren.design>
2019-05-06 16:52:03 +0200

    Add own color scheme to hyper and remove themes

--- hyper.js
+++ hyper.js
@@ -1,24 +1,39 @@
+const backgroundColor = '#0e0d0e' // #16161B
+
 module.exports = {
   config: {
-    fontSize: 13,
-    fontFamily: 'Fira Code, "Inconsolata for Powerline", Monaco, monospace',
-    cursorShape: 'BLOCK',
-    padding: '10px 12px',
-    hyperTabs: {
-      tabIconsColored: true,
-      activityColor: 'salmon',
+    updateChannel: 'stable',
+    fontSize: 12,
+    fontFamily: 'SF Mono, Menlo, "Inconsolata for Powerline", monospace',
+    cursorColor: 'rgba(248,28,229,0.8)',
+    foregroundColor: '#fff',
+    backgroundColor: backgroundColor,
+    selectionColor: 'rgba(248,28,229,0.3)',
+    borderColor: '#333',
+    padding: '2px 10px',
+
+    colors: {
+      black: backgroundColor,
+      red: '#d1267f',
+      green: '#00DA90',
+      yellow: '#F8FB3C',
+      blue: '#009CE6',
+      magenta: '#EC26CD',
+      cyan: '#13C7FE',
+      white: '#C7C7C7',
+      lightBlack: '#686868',
+      lightRed: '#FD6F6B',
+      lightGreen: '#67F86F',
+      lightYellow: '#FFFA72',
+      lightBlue: '#009CE6',
+      lightMagenta: '#FD7CFC',
+      lightCyan: '#68FDFE',
+      lightWhite: '#FFFFFF',
     },
   },
 
-  plugins: [
-    'hyper-tabs-enhanced',
-    'hyper-sync-settings',
-    'hypercwd',
-    'hyper-pane',
-    'hyper-search',
-    'verminal',
-  ],
-
+  plugins: ['hypercwd', 'hyper-search', 'hyper-pane'],
+  localPlugins: [],
   keymaps: {
     'window:reload': 'cmd+alt+r',
     'editor:clearBuffer': 'cmd+r',
